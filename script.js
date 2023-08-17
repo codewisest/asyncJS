710d2314453b85122890f30bded096ee01ba10e4
Chijioke <wisestcodes@gmail.com>
2023-08-17 00:40:34 -0400

    waiting time

--- script.js
+++ script.js
@@ -123,11 +123,29 @@ btn.addEventListener('click', function () {
 });
 
 const lotteryPromise = new Promise(function (resolve, reject) {
-  if (Math.random() >= 0.5) {
-    resolve('You win!!!');
-  } else {
-    reject('You lose!!!');
-  }
+  console.log('lottery draw is happening');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You win!!!');
+    } else {
+      reject(new Error('You lose!!!'));
+    }
+  }, 2000);
 });
 
 lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 1 second');
+  });
