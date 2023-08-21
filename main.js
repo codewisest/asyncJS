a83edc929679a7063dd21712bc5814f5b74f6141
Chijioke <wisestcodes@gmail.com>
2023-08-21 18:51:42 -0400

    import export modules

---
+++ main.js
@@ -0,0 +1,11 @@
+// importing module
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+
+// addToCart('bread', 20);
+
+// console.log(price, tq);
+
+import * as ShoppingCart from './shoppingCart.js';
+console.log('importing module');
+
+ShoppingCart.addToCart('bread', 20);
---
+++ shoppingCart.js
@@ -0,0 +1,16 @@
+// exporting module
+
+console.log('exporting module');
+
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq };
